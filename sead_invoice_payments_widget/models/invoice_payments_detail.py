78cf7aea03ea72a172cb1500a2ad31b0fa47056b
fleyvag <43174467+fleyvag@users.noreply.github.com>
2025-05-05 13:31:54 -0500

    Update invoice_payments_detail.py
    s

--- sead_invoice_payments_widget/models/invoice_payments_detail.py
+++ sead_invoice_payments_widget/models/invoice_payments_detail.py
@@ -5,7 +5,18 @@ class InvoicePaymentsDetail(models.Model):
     _name = 'invoice.payments.detail'
     _description = 'Invoice Payments Detail'
 
-    move_id = fields.Many2one('account.move', string='Move')
+    move_id = fields.Many2one(
+        'account.move',
+        string='Move',
+        required=True,
+        ondelete='cascade'
+    )
     journal_name = fields.Char('Journal Name')
-    amount = fields.Float('amount')
-    date = fields.Date('date')
+    amount = fields.Monetary('Amount')
+    currency_id = fields.Many2one(
+        'res.currency',
+        string='Currency',
+        default=lambda self: self.env.company.currency_id.id,
+        required=True
+    )
+    date = fields.Date('Date')
